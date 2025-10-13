5f0d4e013b168aab8da1ecca61d3729a712f3297
airishGuy <airish1219here@gmail.com>
2025-10-13 09:03:53 +0100

    new ca

--- packages/site/abi/ConfidentialTranscriptAddresses.ts
+++ packages/site/abi/ConfidentialTranscriptAddresses.ts
@@ -4,5 +4,5 @@
   By commands: 'npx hardhat deploy' or 'npx hardhat node'
 */
 export const ConfidentialTranscriptAddresses = { 
-  "11155111": { address: "0x2c1E1a2b526076c9C48d9C1F3F9f170e730BAcdf", chainId: 11155111, chainName: "sepolia" }
+  "11155111": { address: "0xe6B2286C2d26B7E7c6840367e8C1B87bbC45fd8D", chainId: 11155111, chainName: "sepolia" }
 };
